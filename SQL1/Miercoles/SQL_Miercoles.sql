ab078989f438c78fad4af92e1ea63194adfa1fd9
Josue Gz Poli <37153438+JosuePoli@users.noreply.github.com>
2020-01-16 00:20:28 -0600

    Archivos empleados en la clase el día miércoles.
    En la presentación encontrarán los temas de Agregación y Agrupación, Comandos ORDER BY y GROUP BY.
    En el archivo "Ejercicios.txt" se encuentran los queries que usamos para explicar lo nuevo en clase.
    
    El archivo script.txt y el archivo SQL_Miercoles.sql es lo mismo. Uno es para que puedan visualizarlo y el .sql es para que lo puedan cargar como un script (archivo) en el entorno Oracle. 
    
    Y finalmente en el archivo preguntas_SQL.txt vienen las preguntas que se trabajaron en la actividad del dia de hoy :)

---
+++ SQL1/Miercoles/SQL_Miercoles.sql
@@ -0,0 +1,497 @@
+CREATE TABLE CancionesPaLlorarComoMiaColuchi(
+ID_Cancion number(2,0),
+Nombre_Cancion varchar(50),
+Artista_Cancion varchar(50),
+Idioma_Cancion varchar(20),
+MeSeLa_Cancion char(6),
+Duracion_Minutos NUMBER(1,0)
+);
+
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+1,
+'Snuff',
+'Slipknot',
+'Inglés',
+'Sí',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+2,
+'Eso ya lo se',
+'Achepe',
+'Español',
+'Maso',
+'3'
+);
+
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+3,
+'Todavía',
+'La Factoria',
+'Español',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+4,
+'Me tengo que ir',
+'Adolescents Orquesta',
+'Español',
+'Maso',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+5,
+'Through the Glass',
+'Stone Sour',
+'Inglés',
+'Sí',
+'5'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+6,
+'Por tu amor',
+'Alacranes Musicales',
+'Español',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+7,
+'As you wish',
+'Alesana',
+'Inglés',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+8,
+'Memorama',
+'Allison',
+'Español',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+9,
+'Ne me quitte pas',
+'Mireille Mathieu',
+'Francés',
+'no',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+10,
+'Cant help falling in love',
+'Elvis Presley',
+'Inglés',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+11,
+'For no one',
+'The Beatles',
+'Inglés',
+'No',
+'2'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+12,
+'Ojalá',
+'Beret',
+'Español',
+'Maso',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+13,
+'Te vi pasar',
+'Nach',
+'Español',
+'No',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+14,
+'For you',
+'Angus and Julia Stone',
+'Inglés',
+'Maso',
+'5'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+15,
+'City of Stars',
+'Ryan Gosling & Emma Stone',
+'Inglés',
+'Sí',
+'2'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+16,
+'Knives and Pens',
+'Black Veil Brides',
+'Inglés',
+'Maso',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+17,
+'Stay together for the kids',
+'Blink 182',
+'Inglés',
+'Maso',
+'2'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+18,
+'Can you feel my heart',
+'Bring Me The Horizon',
+'Inglés',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+18,
+'Follow You',
+'Bring Me The Horizon',
+'Inglés',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+19,
+'Best of me',
+'A Day To Remember',
+'Inglés',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+20,
+'Everythin will be okay',
+'G-Eazy',
+'Inglés',
+'Ño',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+21,
+'La 2da o 3ra peor depresion en mi vida',
+'lngsht',
+'Español',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+22,
+'Rivoshima',
+'Lngsht',
+'Español',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+23,
+'I Dont Miss You At All',
+'Finneas',
+'Inglés',
+'No',
+'2'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+24,
+'99 luftballons',
+'Nena',
+'Aleman',
+'No',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+25,
+'Total Eclipse of the Heart',
+'Bonnie Tayler',
+'Inglés',
+'Sí',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+26,
+'Te regalo',
+'Carla Morrison',
+'Español',
+'Sí',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+27,
+'Total Eclipse of the Heart',
+'Bonnie Tayler',
+'Inglés',
+'Sí',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+28,
+'Hasta la raíz',
+'Natalia Lafourcade',
+'Español',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+29,
+'Que nadie sepa mi sufrir',
+'Lila Downs',
+'Español',
+'Sí',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+30,
+'Nothing was the same',
+'Hotel Brook',
+'Inglés',
+'No',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+31,
+'La casa del café',
+'Capitán Mio',
+'Español',
+'Sí',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+32,
+'Nothing was the same',
+'Hotel Brook',
+'Inglés',
+'No',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+33,
+'Say something',
+'A Great Big world',
+'Inglés',
+'No',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+34,
+'A través del vaso',
+'Grupo Arranke',
+'Español',
+'Sí',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+34,
+'Someone like you',
+'Ice Nine Kills',
+'Inglés',
+'Sí',
+'4'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+35,
+'Need you now',
+'Sparks the Rescue',
+'Inglés',
+'Sí',
+'3'
+);
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+36,
+'Un Peso',
+'J Balvin ft Bad Bunny',
+'Español',
+'Maso',
+'4'
+);     
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+37,
+'Souvenir',
+'León Larregui',
+'Español',
+'Maso',
+'4'
+);     
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+38,
+'Given up',
+'Linkin Park',
+'Inglés',
+'Maso',
+'3'
+); 
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+39,
+'One more light',
+'Linkin Park',
+'Inglés',
+'Sí',
+'4'
+); 
+
+INSERT INTO CancionesPaLlorarComoMiaColuchi VALUES(
+40,
+'Enganchado a ti',
+'Enrique Bunbury',
+'Español',
+'Sí',
+'4'
+); 
+
+
+
+
+
+E J E M P L O S
+-----------------------------------------------------
+CREATE TABLE Estudiante(
+NoCuenta NUMBER(9),
+Nombre VARCHAR(20),
+ApPaterno VARCHAR(20),
+Carrera VARCHAR(30),
+Edad Number(2),
+Semestre Number(2),
+Poblacion NUMBER(2)
+);
+
+INSERT INTO Estudiante VALUES (
+
+);
+
+----------------------------------------------------
+
+SELECT COUNT(column_name)
+FROM table_name
+WHERE condition;
+
+---------------------------------------------------- 
+
+SELECT COUNT(*)
+FROM Estudiante;
+----------------------------------------------------
+
+SELECT * FROM Estudiante;
+
+
+SELECT COUNT(*)
+FROM Estudiante
+WHERE Semestre < 5; 
+
+----------------------------------------------------
+
+SELECT * FROM Estudiante;
+
+
+SELECT COUNT(Edad)
+FROM Estudiantes; 
+
+----------------------------------------------------
+
+SELECT Poblacion, Carrera
+FROM Estudiante
+ORDER BY Semestre
+ASC;
+
+SELECT Poblacion, Carrera
+FROM Estudiante
+ORDER BY Semestre
+DESC;
+
+
+
+---------------------------------------------------
+
+SELECT * FROM Estudiante
+ORDER BY Nombre, Edad; <-- Le va a dar prioridad a la primera columna
+
+-----------------------------------
+
+SELECT * FROM Estudiante
+ORDER BY Carrera ASC, Semestre DESC; 
+
+---------------------------------------------------
+
+Ejercicios: GROUP BY
+
+"buscar el número de estudiantes en cada carrera"
+
+SELECT COUNT(NoCuenta), Carrera
+FROM Estudiante
+GROUP BY Carrera;
+
+"enumera el número de estudiantes en cada carrera, ordenados de mayor a menor"
+
+SELECT COUNT(NoCuenta), Carrera
+FROM Estudiante
+GROUP BY Carrera
+ORDER BY COUNT(Poblacion) DESC;
+
+----------------------------------------------------
+
+Ejercicios: HAVING
+
+"enumera el número de población en cada carrera. Solo incluya carreras con más de 5 estudiantes"
+
+SELECT COUNT(NoCuenta), Carrera
+FROM Estudiante
+GROUP BY Carrera
+HAVING COUNT(NoCuenta) > 5;
+
+----------------------------------------------------
+
+"enumera el número de estudiantes con el semeste actual correspondiente, ordenados de mayor a menor (solo incluye los que se encuentran arriba de 5to semestre)"
+
+SELECT COUNT(Nocuenta), Semestre
+FROM Estudiante
+GROUP BY Semestre
+HAVING COUNT(Nocuenta) > 5
+ORDER BY COUNT(Nocuenta) DESC;
